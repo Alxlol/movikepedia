86fb451e417d90b23d375b1b0cd185ca7336e419
Alexander <94375773+Alxlol@users.noreply.github.com>
2023-07-30 16:15:48 +0200

    fixed searching within a search page

--- src/routes/search/[id]/+page.svelte
+++ src/routes/search/[id]/+page.svelte
@@ -1,7 +1,6 @@
 <script>
 	import SearchResult from '../../../components/SiteComponents/SearchResult.svelte';
 	export let data;
-	export let searchResults = data.searchResult.results;
 </script>
 
-<SearchResult searchResult={searchResults} />
+<SearchResult searchResult={data.searchResult.results} />
